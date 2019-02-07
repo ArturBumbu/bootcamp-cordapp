150f29ef7063d3ea0d797ddd6c08aca171cb68b5
Artur Bumbu <arturbumbu@gmail.com>
2019-02-07 18:53:59 +0000

    implemented getParticipants()

--- src/main/java/java_bootcamp/TokenState.java
+++ src/main/java/java_bootcamp/TokenState.java
@@ -1,11 +1,11 @@
 package java_bootcamp;
 
-import com.google.common.collect.ImmutableList;
 import net.corda.core.contracts.ContractState;
 import net.corda.core.identity.AbstractParty;
 import net.corda.core.identity.Party;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.List;
 
 /* Our state, defining a shared fact on the ledger.
@@ -49,6 +49,6 @@ public class TokenState implements ContractState {
     @NotNull
     @Override
     public List<AbstractParty> getParticipants() {
-        return null;
+        return Arrays.asList(owner, issuer);
     }
 }
